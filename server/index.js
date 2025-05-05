46ed62ce51865b9f54234ca46620eee21f44d9bf
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-05-05 20:03:19 +0600

    second commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 
 app.use(cors());
