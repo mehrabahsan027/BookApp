68af9a85bc07195e6ad2aa1bb16b01a5b852d015
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-05-05 22:44:19 +0600

    added react-query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from './components/Navbar'
-import { BookProvider } from './context/BookContext'
+import { AppWrapper, BookProvider } from './context/BookContext'
 
 export default function App() {
   return (
     <>
 
+<AppWrapper>
+<BookProvider>
 
-      <BookProvider>
+<Navbar />
 
-        <Navbar />
+<main className='min-h-[calc(100vh-100px)] mt-16 roboto'>
+  <Outlet />
+</main>
 
-        <main className='min-h-[calc(100vh-100px)] mt-16 roboto'>
-          <Outlet />
-        </main>
+<footer>footer</footer>
 
-        <footer>footer</footer>
+</BookProvider>
 
-      </BookProvider>
-
+</AppWrapper>
+     
 
 
     </>
