62ee6fb3d0ecd08c6236c381a7268c1826a82bce
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-05-10 17:42:04 +0600

    remove /

--- server/index.js
+++ server/index.js
@@ -233,9 +233,7 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
