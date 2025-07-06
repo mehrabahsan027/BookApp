fd7425dd0d14e3536c0fec4e94d810b3596f4e0e
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-07-07 00:48:25 +0600

    loading

--- client/src/pages/shop/Shop.jsx
+++ client/src/pages/shop/Shop.jsx
@@ -94,7 +94,7 @@ export default function Shop() {
 
         {/* books grid */}
 
-        {books && books?.length > 0 ? (
+        { !loading && books && books?.length > 0 ? (
           <div className="w-full   max-w-screen-xl mx-auto px-4 py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {
               books.map((book) => <BookCard key={book._id} book={book} />)}
