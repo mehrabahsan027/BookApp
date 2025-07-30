e5ea80442c4322d5cf11df32133e632fa206cff7
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-07-30 21:57:02 +0600

    retry 5 times

--- client/src/context/BookContext.jsx
+++ client/src/context/BookContext.jsx
@@ -97,7 +97,7 @@ export const BookProvider = ({ children }) => {
     }
   },
   keepPreviousData: true,
-  retry: 2, // Retry failed requests up to 2 times
+  retry: 5, // Retry failed requests up to 2 times
   staleTime: 1000 * 60, // Data stays fresh for 1 minute
 });
 
